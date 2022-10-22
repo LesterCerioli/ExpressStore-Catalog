ee498743a1e3c0219491d9119fd9bd57e3ab9381
LesterCerioli <MadalenaDiasSec>
2022-10-22 16:56:51 -0300

    fet9#3): Building Data classes into Infrastructure project

---
+++ source/Infrastructure/ExpressStore.Infrastructure.Catalog/Data/ICustomModelBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressStore.Infrastructure.Catalog.Data
+{
+    public interface ICustomModelBuilder
+    {
+        void Build(ModelBuilder modelBuilder);
+    }
+}
---
+++ source/Infrastructure/ExpressStore.Infrastructure.Catalog/Data/IRepositoryWithTypedId.cs
@@ -0,0 +1,27 @@
+﻿using ExpressStore.Infrastructure.Catalog.Models;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressStore.Infrastructure.Catalog.Data
+{
+    public interface IRepositoryWithTypedId<T, TId> where T : IEntityWithTypedId<TId>
+    {
+        IQueryable<T> Query();
+
+        void Add(T entity);
+
+        void AddRange(IEnumerable<T> entity);
+
+        IDbContextTransaction BeginTransaction();
+
+        void SaveChanges();
+
+        Task SaveChangesAsync();
+
+        void Remove(T entity);
+    }
+}
